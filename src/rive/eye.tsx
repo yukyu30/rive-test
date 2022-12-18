0b2305e885b7b021c8ef93aa1c8f300ca896f1e6
yu5 <fukanoyugo+dev@gmail.com>
2022-12-18 23:34:56 +0900

    動作した

--- src/rive/eye.tsx
+++ src/rive/eye.tsx
@@ -1,4 +1,5 @@
-import { useRive } from "@rive-app/react-canvas"
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas"
+import { useEffect, useState } from "react";
 type Props = {
     className?: string
 }
@@ -7,12 +8,37 @@ export const Eye = (props: Props) => {
     const { rive, RiveComponent } = useRive({
         src: "/eye.riv",
         autoplay: true,
+        stateMachines: "State Machine 1"
     });
+    const [maxWidth, setMaxWidth] = useState<number>();
+    const [maxHeight, setMaxHeight] = useState<number>();
+
+    const xAxisInput = useStateMachineInput(rive, "LookAround", "xAxis", 0);
+    const yAxisInput = useStateMachineInput(rive, "LookAround", "yAxis", 0);
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        if (body) {
+            const bodyRect = body.getBoundingClientRect();
+            setMaxWidth(bodyRect.right);
+            setMaxHeight(bodyRect.bottom);
+        }
+    }, []);
+
+    useEffect(() => {
+        const update = (e: { x: number; y: number; }) => {
+            if (maxWidth && maxHeight && yAxisInput && xAxisInput) {
+                xAxisInput.value = (e.x / maxWidth) * 100;
+                yAxisInput.value = 100 - (e.y / maxHeight) * 100;
+            }
+        };
+        window.addEventListener("mousemove", update);
+        return () => {
+            window.removeEventListener("mousemove", update);
+        };
+    }, [xAxisInput, yAxisInput, maxHeight, maxWidth]);
+
     return (
-        <RiveComponent
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
-            className={props.className}
-        />
+        <RiveComponent />
     );
 }
