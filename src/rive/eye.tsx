6a70a6e540aa90dc158fc656bf8505bb38010329
yu5 <fukanoyugo+dev@gmail.com>
2022-12-18 23:56:52 +0900

    リファクタ

--- src/rive/eye.tsx
+++ src/rive/eye.tsx
@@ -1,7 +1,7 @@
 import { useRive } from "@rive-app/react-canvas"
 
 export const Eye = () => {
-    const { rive, RiveComponent } = useRive({
+    const { RiveComponent } = useRive({
         src: "/eye.riv",
         autoplay: true,
         stateMachines: "State Machine 1" /* ステートマシンの名前が必須っぽい */
